6faee2cb6c5416e2616832e0a1d0a47b947861f0
essebas <sebas.camargo81@gmail.com>
2018-09-03 18:01:44 -0500

    Jframe principal agregado.

--- ALex_Compiladores/src/com/ucentral/edu/vista/ALex_Compiladores.java
+++ ALex_Compiladores/src/com/ucentral/edu/vista/ALex_Compiladores.java
@@ -16,9 +16,8 @@ public class ALex_Compiladores {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         ManejadorArchivos mn = new ManejadorArchivos();
         mn.leerFichero();
-    }
-    
+    } */   
 }
